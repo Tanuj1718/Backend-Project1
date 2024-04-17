3bc4cebe788f9bfad1fe5197f0c333c1c3b33710
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-04-17 12:54:11 +0530

    Data extraction and cors

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"})) //agar json m data aayega toh
+app.use(express.urlencoded({extended: true, limit: "16kb"})) // agar url se data aayega toh
+app.use(express.static("public")) //file pdf images store krke k liye
+app.use(cookieParser()) //cookie se data lene k liye
+
+
+
+
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -6,5 +6,13 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !", err);
+});
 
