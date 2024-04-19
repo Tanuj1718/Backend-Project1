407528b3e1d76578d290c00fd29a4d613b99d4b0
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-04-19 10:04:03 +0530

    setup configured and user route

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 
 
-const app = express()
+const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -14,6 +14,15 @@ app.use(express.json({limit: "16kb"})) //agar json m data aayega toh
 app.use(express.urlencoded({extended: true, limit: "16kb"})) // agar url se data aayega toh
 app.use(express.static("public")) //file pdf images store krke k liye
 app.use(cookieParser()) //cookie se data lene k liye
+
+
+
+//routes
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter) //router alag file m j isliye use likh rhe h wrna get likhte h
+// eg. of route-> https://localhost:8000/api/v1/users/register
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
